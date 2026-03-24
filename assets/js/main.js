9e86bac736b43472128d1d28d9ba4257d32f1d2e
Abhishek Kumar Sharma <mailtoabhisharma96@gmail.com>
2026-03-24 11:36:38 +0530

    Add ed

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Smooth scroll for anchors
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', function(e){
+    const target = document.querySelector(this.getAttribute('href'));
+    if(target){
+      e.preventDefault();
+      target.scrollIntoView({behavior:'smooth'});
+    }
+  })
+});
